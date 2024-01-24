06fb95e127335cd14783a0024ed4b30b6e83f49d
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-01-24 10:44:45 -0900

    Gradient descent implemented

--- regression.py
+++ regression.py
@@ -5,8 +5,9 @@ Date: 1/22/2024
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-def graph_data(data: [(float, float)]):
+def graph_data(data: [(float, float)], line_x:np.array = None, line_y:np.array = None):
     """
     @brief      Graphs data in scatter plot
     @param data      Array of tuples [(x,y), (x,y), ...]
@@ -14,6 +15,10 @@ def graph_data(data: [(float, float)]):
 
     x, y = zip(*data)  # Breaks into two arrays
     plt.scatter(x, y)
+
+    if line_x.any() and line_y.any():
+        plt.plot(line_x, line_y, '-r', label='y=mx+b')
+
     plt.show()
 
 
@@ -22,21 +27,43 @@ def regression(data: [(float, float)]):
     @brief      Finds best fit line for data 
     @param data      Array of tuples [(x,y), (x,y), ...]
     """
-    m, b, total_error = 0
+    m, b = 0, 0
 
-    for data_point in data:
-        x = data_point[0]
-        y = data_point[1]
+    LEARNING_RATE = 0.01
+    ITERATIONS = 500
+    
+    for _ in range(ITERATIONS):
 
-        y_hat = m * x + b 
+        total_error = 0
+        m_derivative, b_derivative = 0, 0
+        for data_point in data:
+            x = data_point[0]
+            y = data_point[1]
 
-        total_error += (y_hat - y) ** 2
+            y_hat = m * x + b 
 
-    n = len(data)    
-    mse = total_error / n  # Mean Squared Error
+            m_derivative += (m * x + b - y) * x
+            b_derivative += (m * x + b - y)
+            
+            total_error += (y_hat - y) ** 2
+
+        
+        n = len(data)    
+        mse = total_error / (2*n)  # Mean Squared Error over 2 (to make derivative easier)
+        print("MSE:", mse, ", m: ", m, ", b: ", b)
+        m_derivative /= n
+        b_derivative /= n
+
+        m = m - LEARNING_RATE * m_derivative
+
+        b = b - LEARNING_RATE * b_derivative
+
+    return m, b
 
 
-    # TODO Add gradient descent
+
+    
+
 
 
 
@@ -52,8 +79,12 @@ def main():
         (6,173)
     ]
 
-    graph_data(data)
-    regression(data)
+    #graph_data(data)
+    m,b =regression(data)
+    
+    line_x = np.linspace(0, 10, 100)
+    line_y = m * line_x + b
+    graph_data(data, line_x, line_y)
 
 if __name__ == "__main__":
     main()
